cf12f4a0e2932c52f1c8a6482964c9c44bc9def8
nicklas.osterbacka <nicklas.osterbacka@gmail.com>
2022-10-28 17:10:28 +0200

    Updated GSL version in installer script

--- C-introduction/C2/install_gsl.sh
+++ C-introduction/C2/install_gsl.sh
@@ -10,9 +10,9 @@ install() {
     tmpdir="$(mktemp -d)"
     pushd "$tmpdir"
 
-    wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz || die "Failed downloading archive"
-    tar -xvf gsl-2.6.tar.gz || die "Failed extracting archive"
-    pushd gsl-2.6/
+    wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.7.tar.gz || die "Failed downloading archive"
+    tar -xvf gsl-2.7.tar.gz || die "Failed extracting archive"
+    pushd gsl-2.7/
     ./configure --prefix="$HOME/.local" || die "Failed configure"
     make || die "Failed compilation"
     make install || die "Failed install"
