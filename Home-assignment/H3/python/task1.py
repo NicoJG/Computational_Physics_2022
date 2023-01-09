23d614491ba9baec8a40face9855a8337e290649
Nico Guth <nico.guth@tu-dortmund.de>
2023-01-09 17:04:27 +0100

    H3 task3 but with a bug

--- Home-assignment/H3/python/task1.py
+++ Home-assignment/H3/python/task1.py
@@ -1,21 +1,96 @@
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 i_step, tau, N, E_T = np.genfromtxt("data/task1.csv", delimiter=",", unpack=True)
 
+# get header metadata
+with open("data/task1.csv", "r") as file:
+    metadata_str = "".join([file.readline() for i in range(1)])
+    metadata_str = metadata_str.replace("# ","")
+    metadata = json.loads(metadata_str)
+    
+n_eq_steps = metadata["n_eq_steps"]
+dtau = metadata["dtau"]
+
 E0_exact = 3/8
 
-plt.figure(figsize=(8,4))
-plt.plot(i_step, N)
-plt.xlabel("iteration step")
-plt.ylabel("$N$ (number of walkers)")
-plt.tight_layout()
-plt.savefig("plots/task1_N.pdf")
+E_mean = E_T[n_eq_steps:].mean()
+E_std = E_T[n_eq_steps:].std()
 
-plt.figure(figsize=(8,4))
-plt.axhline(E0_exact, color="k", alpha=0.5, linestyle="--")
-plt.plot(i_step, E_T)
-plt.xlabel("iteration step")
-plt.ylabel("$E_T \: / \: $ (a.u.)")
+N_mean = N[n_eq_steps:].mean()
+N_std = N[n_eq_steps:].std()
+
+fig, axs = plt.subplots(2,1,figsize=(8,8))
+
+plt.sca(axs[0])
+plt.axvline(tau[n_eq_steps], color="k", alpha=0.5, linestyle="--")
+plt.axhline(N_mean, color="C0", linestyle=":", label=f"$<N> = {N_mean:.0f} \\pm {N_std:.1f}$")
+plt.plot(tau, N, color="C0")
+plt.xlabel(r"$\tau \: / \:$ a.u.")
+plt.ylabel(r"$N \: / \:$ number of walkers")
+plt.legend()
+
+plt.sca(axs[1])
+plt.axvline(tau[n_eq_steps], color="k", linestyle="--")
+plt.axhline(E_mean, color="C1", linestyle=":", label=f"$<E_T> = {E_mean:.4f} \\pm {E_std:.4f}$")
+plt.axhline(E0_exact, color="k", alpha=0.5, linestyle=":", label=f"$E_0 = {E0_exact:.4f}$")
+plt.plot(tau, E_T, color="C1")
+plt.xlabel(r"$\tau \: / \:$ a.u.")
+plt.ylabel(r"$E_T \: / \: $ a.u.")
+plt.legend()
+
 plt.tight_layout()
-plt.savefig("plots/task1_E_T.pdf")
+plt.savefig("plots/task1.pdf")
+
+# %%
+# plot the distribution during the timesteps
+x_data = np.genfromtxt("data/task1_x.csv", delimiter=",")
+
+i_step = x_data[:,0]
+x_2d = x_data[:,1:]
+
+i_step_2d = np.tile(i_step,(x_2d.shape[1],1)).T
+
+mask = ~np.isnan(x_2d.flatten())
+x = x_2d.flatten()[mask]
+i_step = i_step_2d.flatten()[mask]
+
+tau = i_step*dtau
+# %%
+fig, axs = plt.subplots(1,2, gridspec_kw={"width_ratios":[0.8,0.2]}, figsize=(10,4), sharey=True)
+# plot the evolution of the distribution
+plt.sca(axs[0])
+plt.hist2d(tau, x, bins=(x_2d.shape[0], 40), rasterized=True)
+plt.axvline(n_eq_steps*dtau, color="white", alpha=0.7, linestyle="--")
+plt.xlabel(r"$\tau \: / \:$ a.u.")
+plt.ylabel(r"$x \: / \:$ a.u.")
+
+x_min = x.min()
+x_max = x.max()
+
+x_lin = np.linspace(x_min, x_max, 1000)
+phi0 = np.sqrt(2) * np.exp(-np.exp(-x_lin)-x_lin/2)
+
+mask2 = i_step>=n_eq_steps
+weights = 1./N[i_step.astype(int)]
+
+mean_hist, bin_edges = np.histogram(x[mask2], bins=40, weights=weights[mask2], density=True)
+bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
+
+#mean_hist = 
+plt.sca(axs[1])
+plt.plot(phi0**2,x_lin)
+plt.hist(bin_centers, bins=bin_edges, weights=mean_hist, orientation="horizontal")
+plt.xlabel("$\Phi_0$")
+plt.ylabel(r"$x \: / \:$ a.u.")
+axs[1].yaxis.set_tick_params(labelright=True)
+axs[1].yaxis.tick_right()
+axs[1].yaxis.set_label_position("right")
+
+plt.tight_layout()
+plt.savefig("plots/task1_dist.pdf")
+
+
+# %%
--- Home-assignment/H3/src/run.c
+++ Home-assignment/H3/src/run.c
@@ -15,7 +15,7 @@ void task1(gsl_rng* rng) {
     printf("Perform Task 1...\n");
     // initialize walkers
     int N0 = 200; // initial number of walkers
-    int N_max = 10*N0; // maximum number of walkers
+    int N_max = 3*N0; // maximum number of walkers
     double x_min = -5;
     double x_max = 5;
     double dx = (x_max - x_min)/(N0-1);
@@ -32,10 +32,13 @@ void task1(gsl_rng* rng) {
 
     // simulation constants
     double E_T = 0.5;
+    //E_T = 3./8.;
     double gamma = 0.5;
     double dtau = 0.02;
     double sqrt_dtau = sqrt(dtau);
-    int n_steps = 400; // plus the step 0
+    int n_eq_steps = 750;
+    int n_prod_steps = 3250;
+    int n_steps = n_eq_steps + n_prod_steps; // plus the step 0
 
     int* N_arr = (int*)malloc((n_steps+1)*sizeof(int));
     double* E_T_arr = (double*)malloc((n_steps+1)*sizeof(double));
@@ -50,7 +53,6 @@ void task1(gsl_rng* rng) {
     // because the number of walkers changes
     double* temp_x = (double*)calloc(N_max, sizeof(double));
 
-    print_progress(0,0, n_steps, true);
     // do diffusion monte carlo without importance sampling
     // step through imaginary time 
     for (int i_step=1; i_step<n_steps+1; i_step++) {
@@ -73,7 +75,7 @@ void task1(gsl_rng* rng) {
                 }
             }
         }
-        N = i_temp+1;
+        N = i_temp;
         // copy temp_x to x so that we have the new list of walkers
         for (int i_walker=0; i_walker<N; i_walker++) {
             x[i_walker] = temp_x[i_walker];
@@ -84,20 +86,29 @@ void task1(gsl_rng* rng) {
         }
 
         // adjustment of E_T
-        E_T = E_T_sum/i_step + gamma*log(((double)N)/N0);
+        if (i_step < n_eq_steps+1) {
+            // in equilibration
+            E_T = E_T_sum/i_step - gamma*log(((double)N)/N0);
+        } else {
+            if (i_step == n_eq_steps+1) {
+                // first step of production
+                E_T_sum = E_T;
+            }
+            // in production
+            E_T = E_T_sum/(i_step-n_eq_steps) - gamma*log(((double)N)/N0);
+        }
+        E_T_sum += E_T;
         
         // save the results
         N_arr[i_step] = N;
         E_T_arr[i_step] = E_T;
         constant_multiplication(x_arr[i_step], x, 1., N_max);
-        
-        print_progress(i_step, 0, n_steps, false);
     }
 
     printf("Save results of Task 1...\n");
     // save results to files
     FILE* file = fopen("data/task1.csv", "w");
-    fprintf(file, "# {\"gamma\":%.5e, \"dtau\":%.5e}\n", gamma, dtau);
+    fprintf(file, "# {\"gamma\":%.5e, \"dtau\":%.5e, \"n_eq_steps\":%i}\n", gamma, dtau, n_eq_steps);
     fprintf(file, "# i_step, tau, N_walkers, E_T\n");
     for (int i_step=0; i_step<n_steps+1; i_step++) {
         double tau = i_step*dtau;
@@ -105,16 +116,16 @@ void task1(gsl_rng* rng) {
     }
     fclose(file);
     file = fopen("data/task1_x.csv", "w");
-    fprintf(file, "# {\"gamma\":%.5e, \"dtau\":%.5e}\n", gamma, dtau);
+    fprintf(file, "# {\"gamma\":%.5e, \"dtau\":%.5e, \"n_eq_steps\":%i}\n", gamma, dtau, n_eq_steps);
     fprintf(file, "# i_step");
     for (int i_walker=0; i_walker<N_max; i_walker++) {
-        fprintf(file, ", x_%05i", i_walker);
+        fprintf(file, ", x_%06i", i_walker);
     }
     fprintf(file, "\n");
     for (int i_step=0; i_step<n_steps+1; i_step++) {
-        fprintf(file, "%5i", i_step);
+        fprintf(file, "%8i", i_step);
         for (int i_walker=0; i_walker<N_max; i_walker++) {
-            fprintf(file, ", %.5f", x_arr[i_step][i_walker]);
+            fprintf(file, ", %8.5f", x_arr[i_step][i_walker]);
         }
         fprintf(file, "\n");
     }
@@ -125,6 +136,294 @@ void task1(gsl_rng* rng) {
     free(N_arr);
     free(E_T_arr);
     destroy_2D_array(x_arr);
+}
+
+void init_helium_walker(double* pos, gsl_rng* rng) {
+    // initialize one walker r as described in task 2
+    for (int i=0; i<2; i++) {
+        double r = 0.7 + gsl_rng_uniform(rng);
+        double theta = acos(2*gsl_rng_uniform(rng) - 1);
+        double phi = 2*M_PI*gsl_rng_uniform(rng);
+        double x = r*sin(theta)*cos(phi);
+        double y = r*sin(theta)*sin(phi);
+        double z = r*cos(theta);
+        pos[0+i*3] = x;
+        pos[1+i*3] = y;
+        pos[2+i*3] = z;
+    }
+}
+
+double V_helium(double* R) {
+    double r1 = vector_norm(R,3);
+    double r2 = vector_norm(R+3,3);
+    double r12 = 0;
+    for (int i=0; i<3; i++) {
+        r12 += (R[i]-R[i+3])*(R[i]-R[i+3]);
+    }
+    r12 = sqrt(r12);
+    return - 2/r1 - 2/r2 + 1/r12;
+}
+
+void task2(gsl_rng* rng, double*** R_out, int* N_out, int* N_max_out, double* E_T_out) {
+    printf("Perform Task 2...\n");
+    // initialize walkers
+    int N0 = 1000; // initial number of walkers
+    int N_max = 10*N0; // maximum number of walkers
+    
+    // walker array for each walker: (x1,y1,z1,x2,y2,z2)
+    double** R = create_2D_array(N_max, 6);
+    int N = N0;
+    // initialize N walkers
+    for (int i_walker=0; i_walker<N; i_walker++) {
+        init_helium_walker(R[i_walker], rng);
+    }
+
+    // simulation constants
+    double E_T = -3.0;
+    double gamma = 0.5;
+    double dtau = 0.01;
+    double sqrt_dtau = sqrt(dtau);
+    int n_eq_steps = 1000;
+    int n_prod_steps = 4000;
+    int n_steps = n_eq_steps + n_prod_steps; // plus the step 0
+
+    int* N_arr = (int*)malloc((n_steps+1)*sizeof(int));
+    double* E_T_arr = (double*)malloc((n_steps+1)*sizeof(double));
+
+    N_arr[0] = N;
+    E_T_arr[0] = E_T;
+
+    double E_T_sum = E_T; // sum all previous E_T for the cummulative average
+    // we need a temporary array of walkers in each step
+    // because the number of walkers changes
+    double** temp_R = create_2D_array(N_max, 6);
+
+    // do diffusion monte carlo without importance sampling
+    // step through imaginary time 
+    for (int i_step=1; i_step<n_steps+1; i_step++) {
+        int i_temp = 0;
+        for (int i_walker=0; i_walker<N; i_walker++) {
+            // diffusive part
+            for (int i_coord=0; i_coord<6; i_coord++) {
+                R[i_walker][i_coord] = R[i_walker][i_coord] + sqrt_dtau*gsl_ran_gaussian(rng, 1);
+            }
+
+            // reactive part
+            double W = exp(-(V_helium(R[i_walker]) - E_T)*dtau);
+            int m = (int)(W + gsl_rng_uniform(rng));
+            // copy the walker m times into temp_x for the next step
+            for (int i_copy=0; i_copy<m; i_copy++) {
+                for (int i_coord=0; i_coord<6; i_coord++) {
+                    temp_R[i_temp][i_coord] = R[i_walker][i_coord];
+                }
+                i_temp++;
+
+                if (i_temp >= N_max) {
+                    printf("ERROR: there are too many walkers!\n");
+                    exit(1);
+                }
+            }
+        }
+        N = i_temp;
+        // copy temp_R to R so that we have the new list of walkers
+        for (int i_walker=0; i_walker<N; i_walker++) {
+            for (int i_coord=0; i_coord<6; i_coord++) {
+                R[i_walker][i_coord] = temp_R[i_walker][i_coord];
+            }
+        }
+
+        // adjustment of E_T
+        if (i_step < n_eq_steps+1) {
+            // in equilibration
+            E_T = E_T_sum/i_step - gamma*log(((double)N)/N0);
+        } else {
+            if (i_step == n_eq_steps+1) {
+                // first step of production
+                E_T_sum = E_T;
+            }
+            // in production
+            E_T = E_T_sum/(i_step-n_eq_steps) - gamma*log(((double)N)/N0);
+        }
+        E_T_sum += E_T;
+        
+        // save the results
+        N_arr[i_step] = N;
+        E_T_arr[i_step] = E_T;
+    }
+
+    printf("Save results of Task 2...\n");
+    // save results to files
+    FILE* file = fopen("data/task2.csv", "w");
+    fprintf(file, "# {\"gamma\":%.5e, \"dtau\":%.5e, \"n_eq_steps\":%i}\n", gamma, dtau, n_eq_steps);
+    fprintf(file, "# i_step, tau, N_walkers, E_T\n");
+    for (int i_step=0; i_step<n_steps+1; i_step++) {
+        double tau = i_step*dtau;
+        fprintf(file, "%i, %.10f, %i, %.10f\n", i_step, tau, N_arr[i_step], E_T_arr[i_step]);
+    }
+    fclose(file);
+
+    destroy_2D_array(temp_R);
+    free(N_arr);
+    free(E_T_arr);
+    
+    *R_out = R;
+    *N_out = N;
+    *N_max_out = N_max;
+    *E_T_out = E_T;
+}
+
+void perform_drift_part(double* R, double alpha, double dtau) {
+    double v_F[6] = {0};
+    double temp[3];
+    double temp2[3];
+    double temp3[6];
+
+    double r1[3] = {R[0], R[1], R[2]};
+    double r2[3] = {R[3], R[4], R[5]};
+
+    double r12[3];
+    constant_multiplication(temp, r1, -1., 3);
+    elementwise_addition(r12, r2, temp, 3);
+
+    double r12_len = vector_norm(r12, 3);
+    normalize_vector(r12, 3);
+
+    constant_multiplication(temp, r1, -2., 3);
+    constant_multiplication(temp2, r12, -1/(2*(1 + 1*alpha*r12_len)*(1 + 1*alpha*r12_len)), 3);
+    elementwise_addition(v_F, temp, temp2, 3);
+
+    constant_multiplication(temp, r2, -2., 3);
+    constant_multiplication(temp2, r12, +1/(2*(1 + 1*alpha*r12_len)*(1 + 1*alpha*r12_len)), 3);
+    elementwise_addition(v_F+3, temp, temp2, 3);
+
+    constant_multiplication(temp3, v_F, dtau, 6);
+    elementwise_addition(R, temp3, R, 6);
+}
+
+double E_L(double* R, double alpha) {
+    double temp[3];
+    double temp2[3];
+
+    double r1[3] = {R[0], R[1], R[2]};
+    double r2[3] = {R[3], R[4], R[5]};
+
+    double r12_vec[3];
+    constant_multiplication(temp, r2, -1., 3);
+    elementwise_addition(r12_vec, r1, temp, 3);
+
+    double r12 = vector_norm(r12_vec, 3);
+    normalize_vector(r12_vec, 3);
+
+    constant_multiplication(temp, r2, -1, 3);
+    elementwise_addition(temp, r1, temp, 3);
+
+    normalize_vector(r1, 3);
+    normalize_vector(r2, 3);
+    constant_multiplication(temp2, r2, -1, 3);
+    elementwise_addition(temp2, r1, temp2, 3);
+
+    double res = -4;
+    res += dot_product(temp,temp2,3)/(r12*(1+alpha*r12)*(1+alpha*r12));
+    res -= 1/(r12*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12));
+    res -= 1/(4*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12)*(1+alpha*r12));
+    res += 1/r12;
+
+    return res;
+}
+
+void task3(gsl_rng* rng, double** R, int N, int N_max, double E_T) {
+    printf("Perform Task 3...\n");
+    int N0 = 1000;
+    // simulation constants
+    double alpha = 0.15;
+    double gamma = 0.5;
+    double dtau = 0.1;
+    double sqrt_dtau = sqrt(dtau);
+    int n_eq_steps = 0;
+    int n_prod_steps = 5000;
+    int n_steps = n_eq_steps + n_prod_steps; // plus the step 0
+
+    int* N_arr = (int*)malloc((n_steps+1)*sizeof(int));
+    double* E_T_arr = (double*)malloc((n_steps+1)*sizeof(double));
+
+    N_arr[0] = N;
+    E_T_arr[0] = E_T;
+
+    double E_T_sum = E_T; // sum all previous E_T for the cummulative average
+    // we need a temporary array of walkers in each step
+    // because the number of walkers changes
+    double** temp_R = create_2D_array(N_max, 6);
+
+    // do diffusion monte carlo without importance sampling
+    // step through imaginary time 
+    for (int i_step=1; i_step<n_steps+1; i_step++) {
+        int i_temp = 0;
+        for (int i_walker=0; i_walker<N; i_walker++) {
+            // diffusive part
+            for (int i_coord=0; i_coord<6; i_coord++) {
+                R[i_walker][i_coord] = R[i_walker][i_coord] + sqrt_dtau*gsl_ran_gaussian(rng, 1);
+            }
+
+            //drift part
+            perform_drift_part(R[i_walker], alpha, dtau);
+
+            // reactive part
+            double W = exp(-(E_L(R[i_walker], alpha) - E_T)*dtau);
+            int m = (int)(W + gsl_rng_uniform(rng));
+            // copy the walker m times into temp_x for the next step
+            for (int i_copy=0; i_copy<m; i_copy++) {
+                for (int i_coord=0; i_coord<6; i_coord++) {
+                    temp_R[i_temp][i_coord] = R[i_walker][i_coord];
+                }
+                i_temp++;
+
+                if (i_temp >= N_max) {
+                    printf("ERROR: there are too many walkers!\n");
+                    exit(1);
+                }
+            }
+        }
+        N = i_temp;
+        // copy temp_R to R so that we have the new list of walkers
+        for (int i_walker=0; i_walker<N; i_walker++) {
+            for (int i_coord=0; i_coord<6; i_coord++) {
+                R[i_walker][i_coord] = temp_R[i_walker][i_coord];
+            }
+        }
+
+        // adjustment of E_T
+        if (i_step < n_eq_steps+1) {
+            // in equilibration
+            E_T = E_T_sum/i_step - gamma*log(((double)N)/N0);
+        } else {
+            if (i_step == n_eq_steps+1) {
+                // first step of production
+                E_T_sum = E_T;
+            }
+            // in production
+            E_T = E_T_sum/(i_step-n_eq_steps) - gamma*log(((double)N)/N0);
+        }
+        E_T_sum += E_T;
+        
+        // save the results
+        N_arr[i_step] = N;
+        E_T_arr[i_step] = E_T;
+    }
+
+    printf("Save results of Task 3...\n");
+    // save results to files
+    FILE* file = fopen("data/task3.csv", "w");
+    fprintf(file, "# {\"gamma\":%.5e, \"dtau\":%.5e, \"n_eq_steps\":%i}\n", gamma, dtau, n_eq_steps);
+    fprintf(file, "# i_step, tau, N_walkers, E_T\n");
+    for (int i_step=0; i_step<n_steps+1; i_step++) {
+        double tau = i_step*dtau;
+        fprintf(file, "%i, %.10f, %i, %.10f\n", i_step, tau, N_arr[i_step], E_T_arr[i_step]);
+    }
+    fclose(file);
+
+    destroy_2D_array(temp_R);
+    free(N_arr);
+    free(E_T_arr);
 }
 
 int
@@ -139,7 +438,14 @@ run(
     gsl_rng* rng = init_rng(42);
 
     task1(rng);
+
+    double** R;
+    int N, N_max;
+    double E_T;
+    task2(rng, &R, &N, &N_max, &E_T);
+    task3(rng, R, N, N_max, E_T);
     
     gsl_rng_free(rng);
+    destroy_2D_array(R);
     return 0;
 }
